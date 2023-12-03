4dbe9d22262907b489d7fa0feda24c77fc8ad135
aya-soghayyer <128791822+aya-soghayyer@users.noreply.github.com>
2023-12-04 00:55:59 +0200

    second update

---
+++ app/src/main/java/com/example/doctorclinicappfinalproject/showdata.java
@@ -0,0 +1,14 @@
+package com.example.doctorclinicappfinalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class showdata extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_showdata);
+    }
+}
